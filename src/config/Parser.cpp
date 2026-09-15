804207defc85ddffe5ee888e587ba93e89455ca5
Gus1331 <gustavooliver.souza@gmail.com>
2026-09-14 23:35:47 -0300

    feat: iterate through tokens

--- src/config/Parser.cpp
+++ src/config/Parser.cpp
@@ -2,6 +2,31 @@
 #include "config/ConfigData.hpp"
 
 Config parse_tokens(std::vector<std::string> tokens) {
-    (void)tokens;
+    if (tokens.empty()) {
+        throw std::runtime_error("Syntax Error: Config file is empty!");
+    }
+
+    std::vector<std::string>::iterator it = tokens.begin();
+    bool location = false;
+    while (it != tokens.end()) {
+        if (!location) {
+            if (*it == "location") {
+                location = true;
+                it++;
+                if (*it != "{")
+                    throw std::runtime_error("Syntax Error: '{' expected.");
+            } else {
+                throw std::runtime_error("Syntax Error: location expected.");
+            }
+        } else {
+            if (*it == "}") {
+                location = false;
+                it++;
+                continue;
+            }
+            // location
+        }
+        it++;
+    }
     return Config();
 }
